2164851e2d0b81242b5bf721fdd1431841d25ff3
Rafly Maulana <52781454+raflymaulana28@users.noreply.github.com>
2022-10-03 12:13:18 +0700

    Update Color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xFFF67952);
+const Color primaryColor = Color(0xFF32BFC5);
 const Color bgColor = Color(0xFFFBFBFD);
 
 const double defaultPadding = 16.0;
